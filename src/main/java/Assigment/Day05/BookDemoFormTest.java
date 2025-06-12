b802d46a797195b9202195818355384aa835b58a
HongNhung-Meo <hongnhungp2502@gmail.com>
2025-06-12 20:04:17 +0700

    HWDay05

--- src/main/java/Assigment/Day05/BookDemoFormTest.java
+++ src/main/java/Assigment/Day05/BookDemoFormTest.java
@@ -1,7 +1,6 @@
 package Assigment.Day05;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
